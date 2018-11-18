3a688704c9149d69d73d44fad5cc02991dd9b366
Yuriy Aksenov <Yura290610@yandex.ru>
2018-11-18 14:30:00 +0300

    Добавил метод для вставки продукта в бд

---
+++ 3_Bots.sql
@@ -0,0 +1,161 @@
+USE [Zebra];
+GO
+
+--SET IDENTITY_INSERT [dbo].[Questions] ON
+
+--SET IDENTITY_INSERT [dbo].[Questions] OFF
+
+CREATE PROCEDURE uspAddProductBot
+( 
+				 @vendorCode nvarchar(50), @brandName nvarchar(50), @categoryName nvarchar(50), @colorName nvarchar(50), @printName nvarchar(50), @styleName nvarchar(50), @price decimal(6, 2), @madeInCountry nvarchar(50), @link nvarchar(100)
+)
+AS
+BEGIN
+	IF EXISTS
+	(
+		SELECT *
+		FROM Product
+		WHERE VendorCode = @vendorCode
+	)
+	BEGIN
+		RETURN;
+	END;
+
+	---- brand 
+	DECLARE @brandId int;
+
+	SELECT @brandId = BrandID
+	FROM dbo.Brand
+	WHERE [Name] = @brandName;
+
+	IF @brandId IS NULL
+	BEGIN
+		INSERT INTO [dbo].[Brand]( [Name], [Site], [LogoUrl] )
+		VALUES( @brandName, '', '' );
+	END;
+
+	SELECT @brandId = @@IDENTITY;
+
+	-- category
+	DECLARE @categoryId int;
+
+	SELECT @categoryId = CategoryID
+	FROM dbo.Category
+	WHERE RussianName = @categoryName;
+
+	IF @categoryId IS NULL
+	BEGIN
+		INSERT INTO [dbo].Category( RussianName )
+		VALUES( @categoryName );
+	END;
+
+	SELECT @categoryId = @@IDENTITY;
+
+	-- color
+	DECLARE @colorId int;
+
+	SELECT @colorId = ColorTypeID
+	FROM dbo.ColorType
+	WHERE RussianName = @colorName;
+
+	IF @colorId IS NULL
+	BEGIN
+		INSERT INTO [dbo].ColorType( RussianName )
+		VALUES( @colorName );
+	END;
+
+	SELECT @colorId = @@IDENTITY;
+
+	-- print
+	DECLARE @printId int;
+
+	SELECT @printId = PrintTypeID
+	FROM dbo.PrintType
+	WHERE [RussianName] = @printName;
+
+	IF @printId IS NULL
+	BEGIN
+		INSERT INTO [dbo].PrintType( RussianName )
+		VALUES( @printName );
+	END;
+
+	SELECT @printId = @@IDENTITY;
+
+	-- style
+	DECLARE @styleId int;
+
+	SELECT @styleId = StyleTypeID
+	FROM dbo.StyleType
+	WHERE [RussianName] = @styleName;
+
+	IF @styleId IS NULL
+	BEGIN
+		INSERT INTO [dbo].StyleType( RussianName )
+		VALUES( @styleName );
+	END;
+
+	SELECT @styleId = @@IDENTITY;
+
+	-- country
+	DECLARE @countryId int;
+
+	SELECT @countryId = CountryID
+	FROM dbo.Country
+	WHERE [RussianName] = @madeInCountry;
+
+	IF @countryId IS NULL
+	BEGIN
+		INSERT INTO [dbo].Country( RussianName )
+		VALUES( @madeInCountry );
+	END;
+
+	SELECT @countryId = @@IDENTITY;
+
+	-- product
+
+	INSERT INTO [dbo].[Product]( [BrandID], [VendorCode], [CategoryID], [StyleTypeID], [PrintTypeID], [Price], [Description], [Link], [MadeInCountryID], [PhotoPreviewUrl], [CreatedDate], [IsDeleted] )
+	VALUES( @brandId, @vendorCode, @categoryId, @styleId, @printId, @price, '', @link, @countryId, '', GETDATE(), 0 );
+
+END;
+
+GO
+
+
+
+CREATE PROCEDURE uspAddProductSize
+( 
+				 @vendorCode nvarchar(50), @russianSize nvarchar(50), @otherCountrySize nvarchar(50), @otherCountrySizeType nvarchar(50)
+)
+AS
+BEGIN
+	IF EXISTS
+	(
+		SELECT *
+		FROM Product
+		WHERE VendorCode = @vendorCode
+	)
+	BEGIN
+		RETURN;
+	END;
+
+	---- brand 
+	DECLARE @sizeTypeId int;
+
+	SELECT @sizeTypeId = SizeTypeID
+	FROM dbo.SizeType
+	WHERE Russian = @russianSize;
+
+	IF @sizeTypeId IS NULL
+	BEGIN
+		INSERT INTO [dbo].SizeType( Russian, OtherCountry, CountyType )
+		VALUES( @russianSize, @otherCountrySize, @otherCountrySizeType);
+	END;
+
+	SELECT @sizeTypeId = @@IDENTITY;
+
+	-- product sizeType
+
+	INSERT INTO [dbo].[Product]( [BrandID], [VendorCode], [CategoryID], [StyleTypeID], [PrintTypeID], [Price], [Description], [Link], [MadeInCountryID], [PhotoPreviewUrl], [CreatedDate], [IsDeleted] )
+	VALUES( @brandId, @vendorCode, @categoryId, @styleId, @printId, @price, '', @link, @countryId, '', GETDATE(), 0 );
+
+END;
