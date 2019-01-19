372ef149f642e88aace6a164dcbd7c542a2c98d4
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-19 16:01:57 +0700

    [Add] Countries

---
+++ Countries.sql
@@ -0,0 +1,23 @@
+USE [Sweater]
+GO
+
+INSERT INTO [dbo].[Country]
+([FlagUrl], [EnglishName], [RussianName])     VALUES
+('', 'Russia', 'Россия'),
+('', 'China', 'Китай'),
+('', 'Italy', 'Италия'),
+('', 'India', 'Индия'),
+('', 'Uzbekistan', 'Узбекистан'),
+('', 'Bulgaria', 'Болгария'),
+('', 'Romania', 'Румыния'),
+('', 'Bangladesh', 'Бангладеш'),
+('', 'Poland', 'Польша'),
+('', 'Turkey', 'Турция'),
+('', 'Morocco', 'Марокко'),
+('', 'Vietnam', 'Вьетнам'),
+('', 'Myanmar', 'Мьянма'),
+('', 'Republic of Macedonia', 'Республика Македония'),
+('', 'Czech Republic', 'Чешская Республика')
+GO
+
+
